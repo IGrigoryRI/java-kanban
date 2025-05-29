18df0a543e4cff07cad3ce945dca5c2707f47304
GrigoriyR <grigoriy.rvanin@yandex.ru>
2025-05-29 20:28:49 +0500

    Решение финального задания спринта 6

--- src/tests/EpicTest.java
+++ src/tests/EpicTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import tasks.Epic;
 import managers.InMemoryTaskManager;
--- src/tests/TaskTest.java
+++ src/tests/TaskTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import managers.InMemoryTaskManager;
 import managers.Status;
