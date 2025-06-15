b181ad10a5c3ea66b326f7d14821036a976dda46
GrigoriyR <grigoriy.rvanin@yandex.ru>
2025-06-15 13:05:56 +0500

    Решение финального проекта спринта 7

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -1,15 +1,22 @@
 package tasks;
 
+import managers.TaskType;
+
 public class SubTask extends Task {
     protected Integer epicId;
 
     public SubTask(String name, String description, int epicId) {
         super(name, description);
         this.epicId = epicId;
+        this.taskType = TaskType.SUBTASK;
     }
 
     public Integer getEpicId() {
         return epicId;
+    }
+
+    public void setEpicId(Integer epicId) {
+        this.epicId = epicId;
     }
 
     @Override
@@ -33,5 +40,6 @@ public class SubTask extends Task {
         this.status = subTask.getStatus();
         this.id = subTask.getId();
         this.epicId = subTask.getEpicId();
+        this.taskType = subTask.getType();
     }
 }
